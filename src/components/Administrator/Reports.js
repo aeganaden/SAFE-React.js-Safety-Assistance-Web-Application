43eab8532af64f7189528dfabd6b82e83c96b75d
Angelo Ganaden Jr <angelojrganaden@gmail.com>
2019-04-07 17:30:35 +0800

    Solved Reports Filters Implementation

--- src/components/Administrator/Reports.js
+++ src/components/Administrator/Reports.js
@@ -14,6 +14,9 @@ import {
   Spin,
   Popconfirm,
   message,
+  Menu,
+  Dropdown,
+  DatePicker,
 } from 'antd';
 import styled from 'styled-components';
 import moment from 'moment';
@@ -22,7 +25,10 @@ import base from '../common/base';
 import { showNotification } from '../common/helpers';
 import { firebaseApp } from '../common/base';
 
+import EMPTY_SVG from '../../assets/vectors/empty.svg';
+
 const { TabPane } = Tabs;
+const { MonthPicker, WeekPicker } = DatePicker;
 
 const StyledCard = styled(Card)`
   margin-left: 10px;
@@ -85,10 +91,12 @@ const fetchImage = async (key) => {
 export class Reports extends Component {
   state = {
     reports: [],
+    reportsCopy: [],
     users: [],
     visible: false,
     urlArr: [],
     loading: false,
+    dateFilter: 0,
   };
 
   componentWillMount() {
@@ -102,6 +110,11 @@ export class Reports extends Component {
       context: this,
       state: 'users',
       asArray: true,
+    });
+
+    base.bindToState('Reports', {
+      context: this,
+      state: 'reportsCopy',
     });
   }
 
@@ -132,7 +145,6 @@ export class Reports extends Component {
       response_date: moment().format('YYYY-MM-DD hh:mm:ss A'),
     };
 
-    // console.log(updatedReport);
     // 1. Take a copy of the current state
     const reportsCopy = { ...this.state.reports };
     //   // 2. Update that state
@@ -191,9 +203,104 @@ export class Reports extends Component {
     });
   };
 
+  changeDateFilter = (e) => {
+    this.setState({
+      dateFilter: e.key,
+    });
+  };
+
+  filterDates = (dateObj, format) => {
+    const { reports } = this.state;
+    let filterDates = {};
+    Object.keys(reports).forEach((key) => {
+      if (
+        moment(reports[key].report_date).format(format) ===
+        moment(dateObj).format(format)
+      ) {
+        filterDates[key] = reports[key];
+      }
+    });
+
+    return filterDates;
+  };
+
+  filterByMonth = (dateObj) => {
+    const filteredReports = this.filterDates(dateObj, 'MM-YYYY');
+    this.setState({ reportsCopy: filteredReports });
+  };
+
+  filterByWeek = (dateObj) => {
+    const filteredReports = this.filterDates(dateObj, 'WW');
+    this.setState({ reportsCopy: filteredReports });
+  };
+
+  filterByDate = (dateObj) => {
+    const filteredReports = this.filterDates(dateObj, 'MM-DD-YYYY');
+    this.setState({ reportsCopy: filteredReports });
+  };
+
+  filterReportTypes = (type) => {
+    const { reportsCopy } = this.state;
+    let filteredReports = {};
+
+    if (type !== 'others') {
+      Object.keys(reportsCopy).forEach((key) => {
+        if (reportsCopy[key].report_type === type) {
+          filteredReports[key] = reportsCopy[key];
+        }
+      });
+    } else {
+      Object.keys(reportsCopy).forEach((key) => {
+        if (
+          reportsCopy[key] !== 'Fire' &&
+          reportsCopy[key] !== 'Brawl' &&
+          reportsCopy[key] !== 'Gunfight' &&
+          reportsCopy[key] !== 'Robbery'
+        ) {
+          filteredReports[key] = reportsCopy[key];
+        }
+      });
+    }
+
+    this.setState({ reportsCopy: filteredReports });
+  };
+
+  reportTypeFilter = (e) => {
+    switch (e.item.props.children[1]) {
+      case 'Fire':
+        this.filterReportTypes('Fire');
+        break;
+
+      case 'Brawl':
+        this.filterReportTypes('Brawl');
+
+        break;
+
+      case 'Gunfight/Gunshot':
+        this.filterReportTypes('Gunfight');
+
+        break;
+      case 'Robbery':
+        this.filterReportTypes('Robbery');
+
+        break;
+
+      default:
+        this.filterReportTypes('others');
+        break;
+    }
+  };
+
   render() {
-    const { reports, users, urlArr, loading } = this.state;
-    console.table(reports);
+    const {
+      reports,
+      reportsCopy,
+      dateFilter,
+      users,
+      urlArr,
+      loading,
+    } = this.state;
+    console.table(reportsCopy);
     let user = {};
     let otherType = '';
     return (
@@ -221,7 +328,7 @@ export class Reports extends Component {
           </Spin>
         </StyledModal>
         <Header name="REPORTS" />
-        <Tabs defaultActiveKey="1" onChange={() => {}}>
+        <Tabs defaultActiveKey="1">
           <TabPane
             tab={`Pending Emergencies (${
               Object.keys(reports).filter((key) => reports[key].response === 0)
@@ -229,6 +336,19 @@ export class Reports extends Component {
             })`}
             key="1"
           >
+            {Object.keys(reports).filter((key) => reports[key].response === 0) <
+              1 && (
+              <Row type="flex" justify="center">
+                <Col span="6">
+                  <img
+                    src={EMPTY_SVG}
+                    style={{ width: '100%', paddingTop: '50px' }}
+                    alt=""
+                  />
+                  <h1>It's seems to be Empty here..</h1>
+                </Col>
+              </Row>
+            )}
             <Row type="flex" gutter={32}>
               {Object.keys(reports)
                 .filter((key) => reports[key].response === 0)
@@ -345,6 +465,19 @@ export class Reports extends Component {
             })`}
             key="2"
           >
+            {Object.keys(reports).filter((key) => reports[key].response === 1) <
+              1 && (
+              <Row type="flex" justify="center">
+                <Col span="6">
+                  <img
+                    src={EMPTY_SVG}
+                    style={{ width: '100%', paddingTop: '50px' }}
+                    alt=""
+                  />
+                  <h1>It's seems to be Empty here..</h1>
+                </Col>
+              </Row>
+            )}
             <Row type="flex" gutter={32}>
               {Object.keys(reports)
                 .filter((key) => reports[key].response === 1)
@@ -438,6 +571,13 @@ export class Reports extends Component {
                             SOLVED (DONE)
                           </StyledButton>
                         </Popconfirm>
+                        <StyledButton
+                          type="secondary"
+                          block
+                          onClick={() => this.showModalImages(key)}
+                        >
+                          VIEW IMAGE
+                        </StyledButton>
                       </StyledCard>
                     </Col>
                   );
@@ -446,27 +586,134 @@ export class Reports extends Component {
           </TabPane>
           <TabPane
             tab={`Solved Emergencies (${
-              Object.keys(reports).filter((key) => reports[key].response === 2)
-                .length
+              Object.keys(reportsCopy).filter(
+                (key) => reportsCopy[key].response === 2,
+              ).length
             })`}
             key="3"
           >
+            <Row type="flex" gutter="24">
+              <Col>
+                <Dropdown
+                  overlay={
+                    <Menu onClick={this.reportTypeFilter}>
+                      <Menu.Item key="1">
+                        <Icon type="minus" style={{ color: 'red' }} />
+                        Fire
+                      </Menu.Item>
+                      <Menu.Item key="2">
+                        <Icon type="minus" style={{ color: 'volcano' }} />
+                        Brawl
+                      </Menu.Item>
+                      <Menu.Item key="3">
+                        <Icon type="minus" style={{ color: 'orange' }} />
+                        Gunfight/Gunshot
+                      </Menu.Item>
+                      <Menu.Item key="4">
+                        <Icon type="minus" style={{ color: 'magenta' }} />
+                        Robbery
+                      </Menu.Item>
+                      <Menu.Item key="5">
+                        <Icon type="minus" style={{ color: 'chocolate' }} />
+                        Others
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <Button style={{ marginLeft: 8 }}>
+                    <Icon type="filter" /> Report Type Filters{' '}
+                    <Icon type="down" />
+                  </Button>
+                </Dropdown>
+              </Col>
+              <Col>
+                <Dropdown
+                  overlay={
+                    <Menu onClick={this.changeDateFilter}>
+                      <Menu.Item key="1">
+                        <Icon type="calendar" />
+                        Filter by Month
+                      </Menu.Item>
+                      <Menu.Item key="2">
+                        <Icon type="calendar" />
+                        Filter by Week
+                      </Menu.Item>
+                      <Menu.Item key="3">
+                        <Icon type="calendar" />
+                        Filter by Day
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <Button style={{ marginLeft: 8 }}>
+                    <Icon type="filter" /> Date Filters <Icon type="down" />
+                  </Button>
+                </Dropdown>
+              </Col>
+
+              <Col>
+                {dateFilter == 1 && (
+                  <MonthPicker
+                    onChange={(e) => this.filterByMonth(e)}
+                    placeholder="Select month"
+                  />
+                )}
+                {dateFilter == 2 && (
+                  <WeekPicker
+                    onChange={(e) => this.filterByWeek(e)}
+                    placeholder="Select week"
+                  />
+                )}
+                {dateFilter == 3 && (
+                  <DatePicker
+                    onChange={(e) => this.filterByDate(e)}
+                    placeholder="Select date"
+                  />
+                )}
+              </Col>
+              <Col>
+                <Button
+                  type="primary"
+                  onClick={() => this.setState({ reportsCopy: reports })}
+                >
+                  {' '}
+                  remove filters
+                </Button>
+              </Col>
+            </Row>
+            {Object.keys(reportsCopy).filter(
+              (key) => reportsCopy[key].response === 2,
+            ) < 1 && (
+              <Row type="flex" justify="center">
+                <Col span="6">
+                  <img
+                    src={EMPTY_SVG}
+                    style={{ width: '100%', paddingTop: '50px' }}
+                    alt=""
+                  />
+                  <center>
+                    <h2>It's seems to be Empty here..</h2>
+                    <h2> Try adjusting your filters.</h2>
+                  </center>
+                </Col>
+              </Row>
+            )}
             <Row type="flex" gutter={32}>
-              {Object.keys(reports)
-                .filter((key) => reports[key].response === 2)
+              {Object.keys(reportsCopy)
+                .filter((key) => reportsCopy[key].response === 2)
                 .sort(
                   (a, b) =>
                     moment(b.report_date).format('x') -
                     moment(a.report_date).format('x'),
                 )
                 .map((key, index) => {
-                  user = users.find((d) => d.email === reports[key].email);
+                  user = users.find((d) => d.email === reportsCopy[key].email);
                   return (
                     <Col span={8} key={index} style={{ marginTop: '10px' }}>
                       <StyledCard>
                         <HeaderWrapper>
                           <StyledHeader>
-                            {moment(reports[key].report_date).format(
+                            {moment(reportsCopy[key].report_date).format(
                               'MMM DD, YYYY - hh:mm A',
                             )}
                           </StyledHeader>
@@ -475,9 +722,9 @@ export class Reports extends Component {
 
                         <StyledTitle>
                           <Icon type="environment" theme="outlined" />{' '}
-                          {!reports[key].incident_place
+                          {!reportsCopy[key].incident_place
                             ? 'Not Defined'
-                            : reports[key].incident_place}
+                            : reportsCopy[key].incident_place}
                         </StyledTitle>
                         <StyledTitle>
                           <Icon type="user" theme="outlined" />{' '}
@@ -486,7 +733,7 @@ export class Reports extends Component {
                             : ''}
                         </StyledTitle>
                         <ReportTypeWrapper>
-                          {reports[key].report_type
+                          {reportsCopy[key].report_type
                             .split(',')
                             .map((type, index) => {
                               switch (type) {
@@ -533,6 +780,13 @@ export class Reports extends Component {
                               }
                             })}
                         </ReportTypeWrapper>
+                        <StyledButton
+                          type="secondary"
+                          block
+                          onClick={() => this.showModalImages(key)}
+                        >
+                          VIEW IMAGE
+                        </StyledButton>
                       </StyledCard>
                     </Col>
                   );
